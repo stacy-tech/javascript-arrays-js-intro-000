eca613e606849bc78bca6843ebfcfa4e82851734
Stacy Daniel <stacydn23@gmail.com>
2020-09-12 00:48:10 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand",
 "kitkat","skittles"]
 
-function addElementToBeginningOfArray(array, element){
+function addElementToBeginningOfArray(array, item){
   var newArray = array;
   newArray.unshift(item);
   return newArray;
